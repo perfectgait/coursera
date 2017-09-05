4bffdb429bc29c7df4cfc16a75153d3aa1580b8f
Matt Rathbun <mrathbun80@gmail.com>
2017-09-05 15:06:10 -0600

    Algorithms Part 1 Princeton 8Puzzle check-in

--- algorithms_part1_princeton_university/8puzzle/Solver.java
+++ algorithms_part1_princeton_university/8puzzle/Solver.java
@@ -13,10 +13,8 @@ import edu.princeton.cs.algs4.StdOut;
 import java.util.LinkedList;
 
 public class Solver {
-    private final Board initial;
-    private SearchNode goal;
-    private boolean isSolvableHasRun;
-    private boolean isSolvable;
+    private int numberOfMoves;
+    private LinkedList<Board> solution;
 
     private class SearchNode implements Comparable<SearchNode> {
         private final Board board;
@@ -32,11 +30,9 @@ public class Solver {
         }
 
         public int compareTo(SearchNode that) {
-            int thatManhattan = that.board.manhattan();
-
-            if (this.manhattan + this.numberOfMoves < thatManhattan + that.numberOfMoves) {
+            if (this.manhattan + this.numberOfMoves < that.manhattan + that.numberOfMoves) {
                 return -1;
-            } else if (this.manhattan + this.numberOfMoves > thatManhattan + that.numberOfMoves) {
+            } else if (this.manhattan + this.numberOfMoves > that.manhattan + that.numberOfMoves) {
                 return 1;
             }
 
@@ -45,92 +41,74 @@ public class Solver {
     }
 
     public Solver(Board initial) {
-        this.initial = initial;
-
-        if (this.isSolvable()) {
-            MinPQ<SearchNode> queue = new MinPQ<>();
-            SearchNode node = new SearchNode(initial, 0, null);
-            queue.insert(node);
-
-            while (!queue.isEmpty() && !node.board.isGoal()) {
-                node = this.runIterationOfAStarAlgorithm(queue);
-            }
-
-            // @TODO Is there a better way to do this?
-            if (node.board.isGoal()) {
-                this.goal = node;
-            }
-        }
-    }
-
-    public boolean isSolvable() {
-        if (this.isSolvableHasRun) {
-            return this.isSolvable;
+        if (initial == null) {
+            throw new IllegalArgumentException("You may not pass a null board");
         }
 
-        this.isSolvableHasRun = true;
-        this.isSolvable = false;
+        boolean solved = false;
 
-        Board twin = this.initial.twin();
+        Board twin = initial.twin();
 
         MinPQ<SearchNode> originalBoardQueue = new MinPQ<>();
         MinPQ<SearchNode> twinBoardQueue = new MinPQ<>();
-        SearchNode originalBoardNode = new SearchNode(this.initial, 0, null);
+        SearchNode originalBoardNode = new SearchNode(initial, 0, null);
         SearchNode twinBoardNode = new SearchNode(twin, 0, null);
 
         originalBoardQueue.insert(originalBoardNode);
         twinBoardQueue.insert(twinBoardNode);
 
-        while (!originalBoardQueue.isEmpty() && !twinBoardQueue.isEmpty()) {
-            originalBoardNode = this.runIterationOfAStarAlgorithm(originalBoardQueue);
-            twinBoardNode = this.runIterationOfAStarAlgorithm(twinBoardQueue);
+        while (!solved && !originalBoardQueue.isEmpty() && !twinBoardQueue.isEmpty()) {
+            originalBoardNode = this.runIterationOfAStarAlgorithm(
+                originalBoardQueue,
+                originalBoardNode
+            );
+            twinBoardNode = this.runIterationOfAStarAlgorithm(
+                twinBoardQueue,
+                twinBoardNode
+            );
 
             if (originalBoardNode.board.isGoal()) {
-                this.isSolvable = true;
+                this.numberOfMoves = originalBoardNode.numberOfMoves;
+                this.solution = new LinkedList<>();
 
-                return true;
+                while (originalBoardNode != null) {
+                    this.solution.addFirst(originalBoardNode.board);
+                    originalBoardNode = originalBoardNode.previous;
+                }
+
+                solved = true;
             }
 
             if (twinBoardNode.board.isGoal()) {
-                this.isSolvable = false;
+                this.numberOfMoves = -1;
 
-                return false;
+                solved = true;
             }
         }
+    }
 
-        return false;
+    public boolean isSolvable() {
+        return this.numberOfMoves >= 0;
     }
 
     public int moves() {
-        if (this.goal != null) {
-            return this.goal.numberOfMoves;
-        }
-
-        return -1;
+        return this.numberOfMoves;
     }
 
     public Iterable<Board> solution() {
-        if (this.goal != null) {
-            SearchNode node = this.goal;
-            LinkedList<Board> path = new LinkedList<>();
-
-            while (node != null) {
-                path.addFirst(node.board);
-                node = node.previous;
-            }
-
-            return path;
+        if (this.numberOfMoves < 0) {
+            return null;
         }
 
-        return null;
+        return this.solution;
     }
 
-    private SearchNode runIterationOfAStarAlgorithm(MinPQ<SearchNode> queue) {
+    private SearchNode runIterationOfAStarAlgorithm(MinPQ<SearchNode> queue, SearchNode previousNode) {
         SearchNode node = queue.delMin();
         Iterable<Board> neighbors = node.board.neighbors();
 
         for (Board neighbor : neighbors) {
-            if (!node.board.equals(neighbor)) {
+            if (previousNode == null || !previousNode.board.equals(neighbor)) {
                 queue.insert(new SearchNode(neighbor, node.numberOfMoves + 1, node));
             }
         }
@@ -152,12 +130,14 @@ public class Solver {
         Solver solver = new Solver(initial);
 
         // print solution to standard output
-        if (!solver.isSolvable())
+        if (!solver.isSolvable()) {
             StdOut.println("No solution possible");
+        }
         else {
             StdOut.println("Minimum number of moves = " + solver.moves());
-            for (Board board : solver.solution())
+            for (Board board : solver.solution()) {
                 StdOut.println(board);
+            }
         }
     }
 }
